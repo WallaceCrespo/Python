74e00e5ac73846d1040cda4f90666ee602489861
Wallace Crespo <wallace123wal@hotmail.com>
2022-10-16 11:15:13 -0300

    Add files via upload

---
+++ exercicioPython1.py
@@ -0,0 +1,9 @@
+nome = input('Qual é seu nome?')
+idade = input('Qual é sua idade?')
+peso = input('Qual o seu peso?')
+
+
+print('Olá ' + nome + ', prazer em te conhecer!')
+print('Sua idade é: ' + idade + ' anos')
+print('Seu peso é: ' + peso + ' Kg')
+print('Até breve!!')
