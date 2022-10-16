cad707bb855cc003bd53ac33a01e727bdf61ab96
Wallace Crespo <wallace123wal@hotmail.com>
2022-10-16 14:15:03 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,17 @@
+login = (input("Nome do usuário:"))
+senha = (input("Digite a senha:"))
+
+list=["Macarrão, arroz, feijão, farinha, leite, ovos, biscoitos, presunto, queijo, massa de tomate, etc." ]
+
+print("Seu Login é:", login)
+print("Sua senha é:", senha)
+
+print("----Carregando sua lista de compras-----")
+
+
+for carregando in range(10, 100):
+        print(carregando)
+
+print("Carregamento concluído!")
+print("Sua lista de compras é:",list)
+
