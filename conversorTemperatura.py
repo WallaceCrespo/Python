a98efe2ae0f737936d00dec448e5cb055032de50
Wallace Crespo <wallace123wal@hotmail.com>
2022-10-22 18:52:57 -0300

    Add files via upload

---
+++ conversorTemperatura.py
@@ -0,0 +1,7 @@
+n = float(input('Digite a temperatura em Cº: '))
+f = n*1.8+32
+k = n+273.15
+
+print('A temperatura em Celsius é:', n,'ºC')
+print('A temperatura em Fahrenheit é:', f,'°F')
+print('A temperatura em kelvin é:', k,'K')
